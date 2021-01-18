a5c2ab2b1a629dd604abaf2c5673e8c92d7b04e2
Dylan Irmak <dylan.irmak@azure.lloydsbank.com>
2021-01-18 23:25:11 +0000

    udpated urls for gck

--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export const callEventsCalendar = async () => {
     
     // need to get a better way of saving Env var
-    const url = 'http://192.168.0.8:9000/events/calendar'
+    // const url = 'http://192.168.0.8:9000/events/calendar'
+    const url = 'http://35.246.69.133:80/events/calendar'
 
     try {
         const { data } = await axios.get(url)
@@ -18,7 +19,8 @@ export const callEventsCalendar = async () => {
 export const callEventDetails = async (eventId) => {
 
     // need to get a better way of saving Env var
-    const url = `http://192.168.0.8:9000/events/details/${eventId}`
+    // const url = `http://192.168.0.8:9000/events/details/${eventId}`
+    const url = `http://35.246.69.133:80/events/details/${eventId}`
 
     try {
         const { data } = await axios.get(url)
@@ -32,7 +34,8 @@ export const callEventDetails = async (eventId) => {
 }
 
 export const callFightersDetails = async (fighterOneId, fighterTwoId) => {
-    const url = `http://192.168.0.8:9000/fighters/?fighterOneId=${fighterOneId}&fighterTwoId=${fighterTwoId}`
+    // const url = `http://192.168.0.8:9000/fighters/?fighterOneId=${fighterOneId}&fighterTwoId=${fighterTwoId}`
+    const url = `http://35.246.69.133:80/fighters/?fighterOneId=${fighterOneId}&fighterTwoId=${fighterTwoId}`
 
     try {
         const { data } = await axios.get(url)
@@ -45,7 +48,8 @@ export const callFightersDetails = async (fighterOneId, fighterTwoId) => {
 }
 
 export const callWeightclassDetails = async (weight) => {
-    const url = `http://192.168.0.8:9000/fighters/weightclass/${weight}`
+    // const url = `http://192.168.0.8:9000/fighters/weightclass/${weight}`
+    const url = `http://35.246.69.133:80/fighters/weightclass/${weight}`
 
     try {
         const { data } = await axios.get(url)
