ef00387cfb6c49cdfc19ecc1d0d2c74961894324
Dylan Irmak <dylan.irmak@azure.lloydsbank.com>
2021-01-31 17:36:28 +0000

    copied over test-app content

--- src/components/molecules/Loader/index.js
+++ src/components/molecules/Loader/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { TextLoader, CirclesLoader } from 'react-native-indicator';
+import { View, StyleSheet, Text, ActivityIndicator } from 'react-native'
 
 const Loader = () => {
     return (
         <View style={styles.Container}>
             <View style={styles.LoadingBar}>
-                <CirclesLoader size={80} dotRadius={15} color='#f00'/>
+                <ActivityIndicator size='large' color='#f00' />
             </View>
             <View style={styles.TextLoader}>
-            <TextLoader text="Calculating" textStyle={styles.Text}/>
+                <Text>Loading</Text>
             </View>
         </View>
     )
@@ -25,8 +24,9 @@ const styles = StyleSheet.create({
         marginBottom: 20
     },
     TextLoader: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 20,
-        marginLeft: 10
     },
     Text: {
         color: 'red',
