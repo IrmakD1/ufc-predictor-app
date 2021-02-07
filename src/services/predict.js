7a96e13d4ca0629cc6a03d5a7f945bf6d73eae0a
Dylan Irmak <dylan.irmak@azure.lloydsbank.com>
2021-02-07 21:14:18 +0000

    added prediction function

--- src/services/predict.js
+++ src/services/predict.js
@@ -1,9 +1,35 @@
+import _ from 'lodash'
 
 const predict = (a, b) => {
-    const fighterA = {...a, num: Math.random()}
-    const fighterB = {...b, num: Math.random()}
+    let fighter1Score = 0
+    let fighter2Score = 0
 
-    return fighterA.num > fighterB.num ? ({winner: fighterA, loser: fighterB}) : ({winner: fighterB, loser: fighterA}) 
+    let fighterA = {...a}
+    let fighterB = {...b}
+
+    fighterA.record = a.record.wins - a.record.losses
+    fighterB.record = b.record.wins - b.record.losses
+    fighterA.knockdowns = a.knockdowns[0]
+    fighterB.knockdowns = b.knockdowns[0]
+    fighterA.striking = a.siginificantStrikePercentage[0]
+    fighterB.striking = b.siginificantStrikePercentage[0]
+    fighterA.takedowns = a.takedownPercentage[0]
+    fighterB.takedowns = b.takedownPercentage[0]
+    fighterA.totalStriking = a.totalStrikesPercentage[0]
+    fighterB.totalStriking = b.totalStrikesPercentage[0]
+
+    _.forEach(fighterA, statA => {
+        _.forEach(fighterB, statB => {
+            if (statA > statB) fighter1Score ++ 
+            if (statB > statA) fighter2Score ++ 
+        })
+    })
+
+    if (fighter1Score > fighter2Score) return ({winner: fighterA, loser: fighterB})
+
+    if (fighter2Score > fighter1Score) return ({winner: fighterB, loser: fighterA}) 
+
+    if (fighter1Score === fighter2Score) return ({winner: fighterB, loser: fighterA}) 
 }
 
 export default predict
